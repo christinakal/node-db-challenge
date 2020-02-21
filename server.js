229d026f5bc7c1fc4b3afa74af9b405c8086c272
christina kaliora <develop@pirron.one>
2020-02-21 18:17:33 +0100

    designed db/ finished migrations and seeds/ tables appearing with data

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
+const ProjectsRouter = require("./projects/projects-router.js");
+
 server.use(express.json());
-server.get("/", (req, res) => res.send("<h1>Welcome to this sprint challenge!</h1>"));
+server.get("/", (req, res) =>
+  res.send("<h1>Welcome to this sprint challenge!</h1>")
+);
+server.use("/api/projects", ProjectsRouter);
 
 module.exports = server;
