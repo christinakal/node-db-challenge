c033629ff14590cfab76983754630451f4b30f42
christina kaliora <develop@pirron.one>
2020-02-21 19:04:56 +0100

    all api working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = express();
 
 const ProjectsRouter = require("./projects/projects-router.js");
 const ResourcesRouter = require("./resources/resources-router.js");
+const TasksRouter = require("./tasks/tasks-router.js");
 
 server.use(express.json());
 server.get("/", (req, res) =>
@@ -10,5 +11,6 @@ server.get("/", (req, res) =>
 );
 server.use("/api/projects", ProjectsRouter);
 server.use("/api/resources", ResourcesRouter);
+server.use("/api/tasks", TasksRouter);
 
 module.exports = server;
